ded0d8c221e0bc6701373cf5ab6b81f0a1ae51c1
LoatmanP <48681293+LoatmanP@users.noreply.github.com>
2021-08-31 23:11:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,12 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+ hide_footer_style = """
+    <style>
+    .reportview-container .main footer {visibility: hidden;}    
+    """
+    st.markdown(hide_footer_style, unsafe_allow_html=True)
+
 class ExponentialClassifier(object):
     def __init__(self, discountRate=-5, rho=.01):
         self.discountRate = discountRate
